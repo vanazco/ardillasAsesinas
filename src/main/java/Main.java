9be72ada9ce0a01d112ab5e75bb871c53b4afcd5
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2019-03-19 17:01:56 +0100

    Ardilla coloured sprites

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
 
         stage.setTitle( "Ez clap" );
 
