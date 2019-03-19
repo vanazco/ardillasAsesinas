d238547876522d0da2549699a9627333b90d20ab
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2019-03-19 17:17:45 +0100

    Trying to show ardilla

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -24,6 +26,17 @@ public class Main extends Application {
 
         Canvas canvas = new Canvas( 800, 600 );
         root.getChildren().add( canvas );
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Image pilota = new Image("Assets/ardillaCen.png");
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                gc.drawImage(pilota, 50, 50);
+            }
+        }.start();
 
         stage.show();
 
